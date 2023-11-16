c81847c89d9d43adc95c4952b77e8402d5df4e50
SergShcherbina <1990sergik27@gmail.com>
2023-11-17 00:31:51 +0300

    add user-writes-message flow

--- src/app.ts
+++ src/app.ts
@@ -7,38 +7,59 @@ const app = express()
 const httpServer = http.createServer(app);
 const io = new Server(httpServer, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: "*",
         methods: ["GET", "POST"],
         allowedHeaders: ["*"],
-        credentials: true
+        credentials: true,
     },
 });
 
 const port = process.env.PORT || 3000
 
-const messages = [
+type MessageType = {
+    message: string;
+    id: string;
+    user: { id: string, name: string },
+}
+const messages: MessageType[] = [
     {message: 'Hi', id: uuidv4(), user: {id: uuidv4(), name: 'Alex'}},
     {message: 'Hello Alex', id: uuidv4(), user: {id: uuidv4(), name: 'Jon'}},
 ]
 
+const usersState = new Map()
+
 app.get('/', (req: Request, res: Response) => {
-    res.send('Server!');
+    res.send('<h1>Server!!!</h1>');
 });
 
 io.on('connection', (socket) => {
+    usersState.set(socket, {id: uuidv4(), name: 'anonym'})
+
+    socket.on('name-send', (userName: string) => {
+        if(userName.trim().length < 2) return
+        usersState.set(socket, {id: uuidv4(), name: userName})
+    })
 
     socket.on('client-message-send', (message: string) => {
-        let messageItem = {
+        if(message.trim().length < 2) return
+        const messageItem = {
             message: message, 
             id: uuidv4(), 
-            user: {id: uuidv4(), name: uuidv4() }
+            user: usersState.get(socket)
         }
         messages.push( messageItem )
-
         io.emit('new-message-send', messageItem)
     });
 
-    socket.emit('init-messages-published', messages)
+    socket.emit('init-messages-published', messages);
+
+    socket.on('writes-message', ()=> {
+        socket.broadcast.emit('user-writes-message', usersState.get(socket))
+    });
+
+    socket.on('disconnect', ()=> {
+        usersState.delete(socket)
+    })
 });
 
 httpServer.listen(port, () => {
